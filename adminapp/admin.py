69117d18e9b95712c82bdd0f7a1cba2d776a7c97
Tien <lvtien2503@example.com>
2024-04-04 13:33:34 +0700

    change requirements and gitignore file

---
+++ adminapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Student)
+admin.site.register(Academic_Program)
+admin.site.register(Academic_Year)
+admin.site.register(Year_Based_Academic_Program)
